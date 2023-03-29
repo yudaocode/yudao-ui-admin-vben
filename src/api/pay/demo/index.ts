063026664c5f7653824f2837bda7f3e2cf14cc96
xingyuv <xingyu4j@vip.qq.com>
2023-03-29 17:50:42 +0800

    feat: add pay demo api

---
+++ src/api/pay/demo/index.ts
@@ -0,0 +1,21 @@
+import { defHttp } from '@/utils/http/axios'
+
+// 获得示例订单分页
+export function getDemoOrderPage(params) {
+  return defHttp.get({ url: '/pay/demo-order/page', params })
+}
+
+// 获得示例订单
+export function getDemoOrder(id: number) {
+  return defHttp.get({ url: '/pay/demo-order/get?id=' + id })
+}
+
+// 创建示例订单
+export function createDemoOrder(data) {
+  return defHttp.post({ url: '/pay/demo-order/create', data })
+}
+
+// 退款示例订单
+export function updateApp(id: number) {
+  return defHttp.put({ url: '/pay/demo-order/refund?id=' + id })
+}
