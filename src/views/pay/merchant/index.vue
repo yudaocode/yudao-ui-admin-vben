c262854a52c9ae3ec41c2bd242a629fde1f7de73
xingyu <xingyu4j@vip.qq.com>
2023-03-29 23:23:51 +0800

    feat: pay merchant

--- src/views/pay/merchant/index.vue
+++ src/views/pay/merchant/index.vue
@@ -1,3 +1,90 @@
 <template>
-  <div>开发中</div>
+  <div>
+    <BasicTable @register="registerTable">
+      <template #toolbar>
+        <a-button type="primary" v-auth="['pay:merchant:create']" :preIcon="IconEnum.ADD" @click="handleCreate">
+          {{ t('action.create') }}
+        </a-button>
+        <a-button type="warning" v-auth="['pay:merchant:export']" :preIcon="IconEnum.EXPORT" @click="handleExport">
+          {{ t('action.export') }}
+        </a-button>
+      </template>
+      <template #bodyCell="{ column, record }">
+        <template v-if="column.key === 'action'">
+          <TableAction
+            :actions="[
+              { icon: IconEnum.EDIT, label: t('action.edit'), auth: 'pay:merchant:update', onClick: handleEdit.bind(null, record) },
+              {
+                icon: IconEnum.DELETE,
+                color: 'error',
+                label: t('action.delete'),
+                auth: 'pay:merchant:delete',
+                popConfirm: {
+                  title: t('common.delMessage'),
+                  placement: 'left',
+                  confirm: handleDelete.bind(null, record)
+                }
+              }
+            ]"
+          />
+        </template>
+      </template>
+    </BasicTable>
+    <MerchantModal @register="registerModal" @success="reload()" />
+  </div>
 </template>
+<script lang="ts" setup name="Merchant">
+import { useI18n } from '@/hooks/web/useI18n'
+import { useMessage } from '@/hooks/web/useMessage'
+import { useModal } from '@/components/Modal'
+import MerchantModal from './MerchantModal.vue'
+import { IconEnum } from '@/enums/appEnum'
+import { BasicTable, useTable, TableAction } from '@/components/Table'
+import { MerchantExportReqVO, deleteMerchant, getMerchantPage, exportMerchant } from '@/api/pay/merchant'
+import { columns, searchFormSchema } from './merchant.data'
+
+const { t } = useI18n()
+const { createConfirm, createMessage } = useMessage()
+const [registerModal, { openModal }] = useModal()
+
+const [registerTable, { getForm, reload }] = useTable({
+  title: '商户列表',
+  api: getMerchantPage,
+  columns,
+  formConfig: { labelWidth: 120, schemas: searchFormSchema },
+  useSearchForm: true,
+  showTableSetting: true,
+  actionColumn: {
+    width: 140,
+    title: t('common.action'),
+    dataIndex: 'action',
+    fixed: 'right'
+  }
+})
+
+function handleCreate() {
+  openModal(true, { isUpdate: false })
+}
+
+function handleEdit(record: Recordable) {
+  openModal(true, { record, isUpdate: true })
+}
+
+async function handleExport() {
+  createConfirm({
+    title: t('common.exportTitle'),
+    iconType: 'warning',
+    content: t('common.exportMessage'),
+    async onOk() {
+      await exportMerchant(getForm().getFieldsValue() as MerchantExportReqVO)
+      createMessage.success(t('common.exportSuccessText'))
+    }
+  })
+}
+
+async function handleDelete(record: Recordable) {
+  await deleteMerchant(record.id)
+  createMessage.success(t('common.delSuccessText'))
+  reload()
+}
+</script>
