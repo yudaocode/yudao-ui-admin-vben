2593c4457f7919366b12fca252b231ea0c72d18e
xingyuv <xingyu4j@vip.qq.com>
2023-03-29 17:42:06 +0800

    perf(BasicTable): 滑块验证码位置

--- src/components/Verifition/src/Verify.vue
+++ src/components/Verifition/src/Verify.vue
@@ -150,9 +150,9 @@ export default {
   background-color: #fff;
   box-shadow: 0 0 10px rgb(0 0 0 / 30%);
   left: 50%;
-  top: 50%;
+  top: 25%;
   border-radius: 5px;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, -25%);
 }
 
 .verifybox-top {
