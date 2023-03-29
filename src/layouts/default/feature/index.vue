286cd991d33b258180362faf000ccffe7d537343
xingyuv <xingyu4j@vip.qq.com>
2023-03-29 17:14:28 +0800

    fix: 路径错误

--- src/layouts/default/feature/index.vue
+++ src/layouts/default/feature/index.vue
@@ -16,9 +16,9 @@ import { useUserStoreWithOut } from '@/store/modules/user'
 import { SettingButtonPositionEnum } from '@/enums/appEnum'
 import { createAsyncComponent } from '@/utils/factory/createAsyncComponent'
 
-import SessionTimeoutLogin from '@/views/sys/login/SessionTimeoutLogin.vue'
+import SessionTimeoutLogin from '@/views/base/login/SessionTimeoutLogin.vue'
 
-const LayoutLockPage = createAsyncComponent(() => import('@/views/sys/lock/index.vue'))
+const LayoutLockPage = createAsyncComponent(() => import('@/views/base/lock/index.vue'))
 
 const SettingDrawer = createAsyncComponent(() => import('@/layouts/default/setting/index.vue'))
 
