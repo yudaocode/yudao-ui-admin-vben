2cf363d6e8e1357462bc6480c3b3e8094d1e1c42
xingyu <xingyu4j@vip.qq.com>
2023-04-12 22:13:41 +0800

    feat: bpm api

--- src/api/base/upload.ts
+++ src/api/base/upload.ts
@@ -2,13 +2,14 @@ import { UploadApiResult } from './model/uploadModel'
 import { defHttp } from '@/utils/http/axios'
 import { UploadFileParams } from '@/types/axios'
 import { useGlobSetting } from '@/hooks/setting'
+import { AxiosProgressEvent } from 'axios'
 
 const { uploadUrl = '' } = useGlobSetting()
 
 /**
  * @description: Upload interface
  */
-export function uploadApi(params: UploadFileParams, onUploadProgress: (progressEvent: ProgressEvent) => void) {
+export function uploadApi(params: UploadFileParams, onUploadProgress: (progressEvent: AxiosProgressEvent) => void) {
   return defHttp.uploadFile<UploadApiResult>(
     {
       url: uploadUrl,
---
+++ src/api/bpm/definition/index.ts
@@ -0,0 +1,13 @@
+import { defHttp } from '@/utils/http/axios'
+
+export function getProcessDefinitionPage(params) {
+  return defHttp.get({ url: '/bpm/process-definition/page', params })
+}
+
+export function getProcessDefinitionList(params) {
+  return defHttp.get({ url: '/bpm/process-definition/list', params })
+}
+
+export function getProcessDefinitionBpmnXML(id) {
+  return defHttp.get({ url: '/bpm/process-definition/get-bpmn-xml?id=' + id })
+}
