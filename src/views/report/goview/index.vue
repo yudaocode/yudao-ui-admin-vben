c74bb8adce52f42734999e58540c515543aa9d2b
xingyuv <xingyu4j@vip.qq.com>
2023-04-13 13:46:38 +0800

    feat: add report views

--- src/views/report/goview/index.vue
+++ src/views/report/goview/index.vue
@@ -1,3 +1,11 @@
 <template>
-  <div>开发中</div>
+  <div>
+    <IFrame :src="src" />
+  </div>
 </template>
+<script setup lang="ts" name="ReportGoview">
+import { ref } from 'vue'
+import { IFrame } from '@/components/IFrame'
+
+const src = ref('http://127.0.0.1:3000')
+</script>
--- src/views/report/jmreport/index.vue
+++ src/views/report/jmreport/index.vue
@@ -1,3 +1,12 @@
 <template>
-  <div>开发中</div>
+  <div>
+    <IFrame :src="src" />
+  </div>
 </template>
+<script setup lang="ts" name="ReportJmreport">
+import { ref } from 'vue'
+import { IFrame } from '@/components/IFrame'
+import { getAccessToken } from '@/utils/auth'
+
+const src = ref(import.meta.env.VITE_GLOB_BASE_URL + '/jmreport/list?token=' + getAccessToken())
+</script>
