ee56231f02898ec9cf4b982a599e71d5d43bf62e
xingyu <xingyu4j@vip.qq.com>
2023-08-11 20:05:24 +0800

    perf: useRefs函数接收泛型类型

--- src/hooks/core/useRefs.ts
+++ src/hooks/core/useRefs.ts
@@ -1,16 +1,20 @@
 import type { Ref } from 'vue'
 import { onBeforeUpdate, ref } from 'vue'
 
-export function useRefs(): [Ref<HTMLElement[]>, (index: number) => (el: HTMLElement) => void] {
-  const refs = ref([]) as Ref<HTMLElement[]>
+function useRefs<T = HTMLElement>(): { refs: Ref<T[]>; setRefs: (index: number) => (el: T) => void } {
+  const refs = ref([]) as Ref<T[]>
 
   onBeforeUpdate(() => {
     refs.value = []
   })
 
-  const setRefs = (index: number) => (el: HTMLElement) => {
+  const setRefs = (index: number) => (el: T) => {
     refs.value[index] = el
   }
 
-  return [refs, setRefs]
+  return {
+    refs,
+    setRefs,
+  }
 }
+export { useRefs }
