aa7a926344dc153ed12044e7441d7148a2229396
xingyu <xingyu4j@vip.qq.com>
2023-08-12 19:31:26 +0800

    fix: infra server

--- src/views/infra/server/index.vue
+++ src/views/infra/server/index.vue
@@ -1,14 +1,29 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 import { IFrame } from '@/components/IFrame'
+import { getConfigKey } from '@/api/infra/config'
 
-defineOptions({ name: 'InfraServer' })
+defineOptions({ name: 'InfraAdminServer' })
 
 const src = ref(`${import.meta.env.VITE_GLOB_BASE_URL}/admin/applications`)
+
+const loading = ref(true)
+
+async function getInfo() {
+  const res = await getConfigKey('url.spring-boot-admin')
+  if (res && res.length !== 0)
+    src.value = res
+
+  loading.value = false
+}
+
+onMounted(() => {
+  getInfo()
+})
 </script>
 
 <template>
   <div>
-    <IFrame :src="src" />
+    <IFrame v-if="!loading" :src="src" />
   </div>
 </template>
