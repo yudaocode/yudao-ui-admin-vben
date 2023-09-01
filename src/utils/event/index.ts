2694c179d2c8a415f03761a7300b40257dcb6f87
xingyu <xingyu4j@vip.qq.com>
2023-09-01 17:22:19 +0800

    fix: initEvent deprecated

--- src/utils/event/index.ts
+++ src/utils/event/index.ts
@@ -36,8 +36,11 @@ export function removeResizeListener(element: any, fn: () => any) {
 }
 
 export function triggerWindowResize() {
-  const event = document.createEvent('HTMLEvents')
-  event.initEvent('resize', true, true)
-  ;(event as any).eventType = 'message'
-  window.dispatchEvent(event)
+  // const event = document.createEvent('HTMLEvents')
+  // event.initEvent('resize', true, true)
+  // ;(event as any).eventType = 'message'
+  // window.dispatchEvent(event)
+
+  const ev = new Event('message', { bubbles: true, cancelable: true })
+  window.dispatchEvent(ev)
 }
