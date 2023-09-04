8030b547955c72b8e1b10e72cd773c2ac6a63b0b
xingyu <xingyu4j@vip.qq.com>
2023-09-04 14:27:17 +0800

    feat(Form): 为fieldMapToTime的映射类型增加时间戳转换

--- src/components/Form/src/hooks/useFormValues.ts
+++ src/components/Form/src/hooks/useFormValues.ts
@@ -107,12 +107,22 @@ export function useFormValues({ defaultValueRef, getSchema, formModel, getProps 
 
       const [startTimeFormat, endTimeFormat] = Array.isArray(format) ? format : [format, format]
 
-      values[startTimeKey] = dateUtil(startTime).format(startTimeFormat)
-      values[endTimeKey] = dateUtil(endTime).format(endTimeFormat)
+      values[startTimeKey] = formatTime(startTime, startTimeFormat)
+      values[endTimeKey] = formatTime(endTime, endTimeFormat)
       Reflect.deleteProperty(values, field)
     }
 
     return values
+  }
+
+  function formatTime(time: string, format: string) {
+    if (format === 'timestamp')
+      return dateUtil(time).unix()
+
+    else if (format === 'timestampStartDay')
+      return dateUtil(time).startOf('day').unix()
+
+    return dateUtil(time).format(format)
   }
 
   function initDefault() {
