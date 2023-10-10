0bdfd88035c38dc8b423be5293ea61bb1ed8b70d
xingyu <xingyu4j@vip.qq.com>
2023-10-10 11:08:00 +0800

    fix: 多选框必填选中校验异常

--- src/components/Form/src/components/ApiSelect.vue
+++ src/components/Form/src/components/ApiSelect.vue
@@ -128,7 +128,7 @@ function emitChange() {
 }
 
 function handleChange(_, ...args) {
-  emit('change', args[0] ? args[0].value : undefined)
+  emit('change', ...args)
   emitData.value = args
 }
 </script>
