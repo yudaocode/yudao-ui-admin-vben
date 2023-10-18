1de5f2bb5aaa37826dc72b1ee51f5dfc44808646
xingyu <xingyu4j@vip.qq.com>
2023-10-18 15:38:12 +0800

    chore: use new eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,11 @@ const antfu = require('@antfu/eslint-config').default
 const unocss = require('@unocss/eslint-plugin')
 
 module.exports = antfu(
-  {},
+  {
+    rules: {
+      'no-console': 'off',
+      'vue/custom-event-name-casing': 'off',
+    },
+  },
   unocss.configs.flat,
 )
