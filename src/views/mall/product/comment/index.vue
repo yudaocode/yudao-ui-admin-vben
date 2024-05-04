daa491caca809af93ec36c662417353c0e81ad55
YunaiV <zhijiantianya@gmail.com>
2024-05-04 22:23:18 +0800

    【新增】mall：相关文档的链接

---
+++ src/views/mall/product/comment/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { DocAlert } from '@/components/DocAlert'
+</script>
+
+<template>
+  <div>
+    <DocAlert title="【商品】商品评价" url="https://doc.iocoder.cn/mall/product-comment/" />
+
+    <a-button danger type="link" target="_blank" href="https://github.com/yudaocode/yudao-ui-admin-vue3">
+      该功能支持 Vue3 + element-plus 版本！
+    </a-button>
+    <br />
+    <a-button type="link" target="_blank" href="https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/mall/product/comment/index.vue">
+      可参考 https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/mall/product/comment/index.vue 代码，pull request 贡献给我们！
+    </a-button>
+  </div>
+</template>
---
+++ src/views/mall/trade/delivery/express/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { DocAlert } from '@/components/DocAlert'
+</script>
+
+<template>
+  <div>
+    <DocAlert title="【交易】快递发货" url="https://doc.iocoder.cn/mall/trade-delivery-express/" />
+
+    <a-button danger type="link" target="_blank" href="https://github.com/yudaocode/yudao-ui-admin-vue3">
+      该功能支持 Vue3 + element-plus 版本！
+    </a-button>
+    <br />
+    <a-button type="link" target="_blank" href="https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/mall/trade/delivery/express/index.vue">
+      可参考 https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/mall/trade/delivery/express/index.vue 代码，pull request 贡献给我们！
+    </a-button>
+  </div>
+</template>
