fe1de0d4adaccdf8029b79befa3b62bf4fa5aac2
YunaiV <zhijiantianya@gmail.com>
2024-05-18 21:17:18 +0800

    新增vben版中的《代码生成案例》

---
+++ src/views/infra/demo/demo03/erp/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { DocAlert } from '@/components/DocAlert'
+</script>
+<template>
+  <div>
+    <DocAlert title="代码生成（主子表）" url="https://doc.iocoder.cn/new-feature/master-sub/" />
+
+    <a-button danger type="link" target="_blank" href="https://github.com/yudaocode/yudao-ui-admin-vue3">
+      该功能支持 Vue3 + element-plus 版本！
+    </a-button>
+    <br />
+    <a-button type="link" target="_blank" href="https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/infra/demo/demo03/erp/index.vue">
+      可参考 https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/infra/demo/demo03/erp/index.vue 代码，pull request 贡献给我们！
+    </a-button>
+  </div>
+</template>
