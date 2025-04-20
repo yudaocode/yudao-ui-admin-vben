6bfc1961ce986471cf900385ce8eef2d7df82634
YunaiV <zhijiantianya@gmail.com>
2025-04-20 08:53:32 +0800

    feat：增加个人中心：30% 支持右侧的基本信息修改

--- apps/web-antd/src/views/_core/profile/index.vue
+++ apps/web-antd/src/views/_core/profile/index.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import type { SystemUserProfileApi } from '#/api/system/user/profile';
 
+import { Card, Tabs } from 'ant-design-vue';
+import { Page } from '@vben/common-ui';
+import ProfileUser from './modules/profile-user.vue';
+import BaseInfo from './modules/base-info.vue';
+
 import { onMounted, ref } from 'vue';
 import { getUserProfile } from '#/api/system/user/profile';
 import { useAuthStore } from '#/store';
-import { Card, Tabs } from 'ant-design-vue';
-import { Page } from '@vben/common-ui';
-
-import ProfileUser from './modules/profile-user.vue';
 
 const authStore = useAuthStore();
 const activeName = ref('basicInfo');
@@ -43,7 +44,7 @@ onMounted(loadProfile);
       <Card class="ml-3 w-3/5">
         <Tabs v-model:active-key="activeName" class="-mt-4">
           <Tabs.TabPane key="basicInfo" tab="基本信息">
-            <!-- <BaseSetting :profile="profile" /> -->
+             <BaseInfo :profile="profile" @success="refreshProfile" />
           </Tabs.TabPane>
           <Tabs.TabPane key="resetPwd" tab="修改密码">
             <!-- <ResetPwd /> -->
