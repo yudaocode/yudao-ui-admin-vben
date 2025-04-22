062111502f7f4c2d0ce7859e1469eb9cdaf5a3fe
xingyu4j <xingyu4j@vip.qq.com>
2025-04-22 11:28:31 +0800

    fix: warn defineProps is a compiler macro and no longer needs to be imported

--- apps/web-antd/src/components/dict-tag/dict-tag.vue
+++ apps/web-antd/src/components/dict-tag/dict-tag.vue
@@ -1,57 +1,72 @@
 <script setup lang="ts">
 // TODO @芋艿：貌似不用 src 目录
-import { computed, defineProps } from 'vue'
-import { Tag } from 'ant-design-vue'
+import { computed } from 'vue';
+
+import { Tag } from 'ant-design-vue';
+
 // import { isHexColor } from '@/utils/color' // TODO @芋艿：【可优化】增加 cssClass 的处理 https://gitee.com/yudaocode/yudao-ui-admin-vben/blob/v2.4.1/src/components/DictTag/src/DictTag.vue#L60
-import { getDictObj } from '#/utils/dict'
+import { getDictObj } from '#/utils/dict';
 
 interface DictTagProps {
   /**
    * 字典类型
    */
-  type: string
+  type: string;
   /**
    * 字典值
    */
-  value: any
+  value: any;
   /**
    * 图标
    */
-  icon?: string
+  icon?: string;
 }
 
-const props = defineProps<DictTagProps>()
+const props = defineProps<DictTagProps>();
 
 /** 获取字典标签 */
 const dictTag = computed(() => {
   // 校验参数有效性
   if (!props.type || props.value === undefined || props.value === null) {
-    return null
+    return null;
   }
 
   // 获取字典对象
-  const dict = getDictObj(props.type, String(props.value))
+  const dict = getDictObj(props.type, String(props.value));
   if (!dict) {
-    return null
+    return null;
   }
 
   // 处理颜色类型
-  let colorType = dict.colorType
-  if (colorType === 'primary') {
-    colorType = 'processing'
-  } else if (colorType === 'danger') {
-    colorType = 'error'
-  } else if (colorType === 'info') {
-    colorType = 'default'
-  } else if (!colorType) {
-    colorType = 'default'
+  let colorType = dict.colorType;
+  switch (colorType) {
+    case 'danger': {
+      colorType = 'error';
+
+      break;
+    }
+    case 'info': {
+      colorType = 'default';
+
+      break;
+    }
+    case 'primary': {
+      colorType = 'processing';
+
+      break;
+    }
+    default: {
+      if (!colorType) {
+        colorType = 'default';
+      }
+    }
   }
 
   return {
     label: dict.label || '',
-    colorType
-  }
-})
+    colorType,
+  };
+});
 </script>
 
 <template>
