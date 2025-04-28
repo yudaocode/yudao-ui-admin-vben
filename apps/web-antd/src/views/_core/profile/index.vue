bb72b91a1d9d7d4226d447d02f906bb2fe58541d
xingyu4j <xingyu4j@vip.qq.com>
2025-04-28 10:17:21 +0800

    fix: style code

--- apps/web-antd/src/views/_core/profile/index.vue
+++ apps/web-antd/src/views/_core/profile/index.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
 import type { SystemUserProfileApi } from '#/api/system/user/profile';
 
-import { Card, Tabs } from 'ant-design-vue';
-import { Page } from '@vben/common-ui';
-import ProfileUser from './modules/profile-user.vue';
-import BaseInfo from './modules/base-info.vue';
-import ResetPwd from './modules/reset-pwd.vue';
-import UserSocial from './modules/user-social.vue';
-
 import { onMounted, ref } from 'vue';
+
+import { Page } from '@vben/common-ui';
+
+import { Card, Tabs } from 'ant-design-vue';
+
 import { getUserProfile } from '#/api/system/user/profile';
 import { useAuthStore } from '#/store';
+
+import BaseInfo from './modules/base-info.vue';
+import ProfileUser from './modules/profile-user.vue';
+import ResetPwd from './modules/reset-pwd.vue';
+import UserSocial from './modules/user-social.vue';
 
 const authStore = useAuthStore();
 const activeName = ref('basicInfo');
@@ -46,13 +49,13 @@ onMounted(loadProfile);
       <Card class="ml-3 w-3/5">
         <Tabs v-model:active-key="activeName" class="-mt-4">
           <Tabs.TabPane key="basicInfo" tab="基本设置">
-             <BaseInfo :profile="profile" @success="refreshProfile" />
+            <BaseInfo :profile="profile" @success="refreshProfile" />
           </Tabs.TabPane>
           <Tabs.TabPane key="resetPwd" tab="密码设置">
-             <ResetPwd />
+            <ResetPwd />
           </Tabs.TabPane>
           <Tabs.TabPane key="userSocial" tab="社交绑定" force-render>
-             <UserSocial @update:active-name="activeName = $event" />
+            <UserSocial @update:active-name="activeName = $event" />
           </Tabs.TabPane>
           <!-- TODO @芋艿：在线设备 -->
         </Tabs>
--- apps/web-antd/src/views/bpm/processInstance/index.vue
+++ apps/web-antd/src/views/bpm/processInstance/index.vue
@@ -1,18 +1,34 @@
 <script lang="ts" setup>
-import { DocAlert } from '#/components/doc-alert';
-import { Button } from 'ant-design-vue';
 import { Page } from '@vben/common-ui';
+
+import { Button } from 'ant-design-vue';
+
+import { DocAlert } from '#/components/doc-alert';
 </script>
 
 <template>
   <Page>
-    <DocAlert title="流程发起、取消、重新发起" url="https://doc.iocoder.cn/bpm/process-instance/" />
-    <Button danger type="link" target="_blank" href="https://github.com/yudaocode/yudao-ui-admin-vue3">
+    <DocAlert
+      title="流程发起、取消、重新发起"
+      url="https://doc.iocoder.cn/bpm/process-instance/"
+    />
+    <Button
+      danger
+      type="link"
+      target="_blank"
+      href="https://github.com/yudaocode/yudao-ui-admin-vue3"
+    >
       该功能支持 Vue3 + element-plus 版本！
     </Button>
     <br />
-    <Button type="link" target="_blank" href="https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/bpm/processInstance/index">
-      可参考 https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/bpm/processInstance/index 代码，pull request 贡献给我们！
+    <Button
+      type="link"
+      target="_blank"
+      href="https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/bpm/processInstance/index"
+    >
+      可参考
+      https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/bpm/processInstance/index
+      代码，pull request 贡献给我们！
     </Button>
   </Page>
 </template>
