1335616fe77088d5e8031694c77c287486fbbf3b
xingyu4j <xingyu4j@vip.qq.com>
2025-04-30 11:25:46 +0800

    feat: pay notify [detail 未测试]

--- apps/web-antd/src/views/pay/notify/index.vue
+++ apps/web-antd/src/views/pay/notify/index.vue
@@ -1,31 +1,79 @@
 <script lang="ts" setup>
-import { Page } from '@vben/common-ui';
+import type {
+  OnActionClickParams,
+  VxeTableGridOptions,
+} from '#/adapter/vxe-table';
 
-import { Button } from 'ant-design-vue';
+import { Page, useVbenModal } from '@vben/common-ui';
 
+import { useVbenVxeGrid } from '#/adapter/vxe-table';
+import * as PayNotifyApi from '#/api/pay/notify';
 import { DocAlert } from '#/components/doc-alert';
+
+import { useGridColumns, useGridFormSchema } from './data';
+import Detail from './modules/detail.vue';
+
+const [NotifyDetailModal, notifyDetailModalApi] = useVbenModal({
+  connectedComponent: Detail,
+  destroyOnClose: true,
+});
+
+/** 刷新表格 */
+function onRefresh() {
+  gridApi.query();
+}
+
+/** 查看详情 */
+function onDetail(row: any) {
+  notifyDetailModalApi.setData(row).open();
+}
+
+/** 表格操作按钮的回调函数 */
+function onActionClick({ code, row }: OnActionClickParams<any>) {
+  switch (code) {
+    case 'detail': {
+      onDetail(row);
+      break;
+    }
+  }
+}
+
+const [Grid, gridApi] = useVbenVxeGrid({
+  formOptions: {
+    schema: useGridFormSchema(),
+  },
+  gridOptions: {
+    columns: useGridColumns(onActionClick),
+    height: 'auto',
+    keepSource: true,
+    proxyConfig: {
+      ajax: {
+        query: async ({ page }, formValues) => {
+          return await PayNotifyApi.getNotifyTaskPage({
+            pageNo: page.currentPage,
+            pageSize: page.pageSize,
+            ...formValues,
+          });
+        },
+      },
+    },
+    rowConfig: {
+      keyField: 'id',
+    },
+    toolbarConfig: {
+      refresh: { code: 'query' },
+      search: true,
+    },
+  } as VxeTableGridOptions<any>,
+});
 </script>
 
 <template>
-  <Page>
-    <DocAlert title="支付功能开启" url="https://doc.iocoder.cn/pay/build/" />
-    <Button
-      danger
-      type="link"
-      target="_blank"
-      href="https://github.com/yudaocode/yudao-ui-admin-vue3"
-    >
-      该功能支持 Vue3 + element-plus 版本！
-    </Button>
-    <br />
-    <Button
-      type="link"
-      target="_blank"
-      href="https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/pay/notify/index"
-    >
-      可参考
-      https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/pay/notify/index
-      代码，pull request 贡献给我们！
-    </Button>
+  <Page auto-content-height>
+    <template #doc>
+      <DocAlert title="支付功能开启" url="https://doc.iocoder.cn/pay/build/" />
+    </template>
+    <NotifyDetailModal @success="onRefresh" />
+    <Grid table-title="支付通知列表" />
   </Page>
 </template>
---
+++ apps/web-antd/src/views/pay/notify/modules/detail.vue
@@ -0,0 +1,107 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+import { useVbenModal } from '@vben/common-ui';
+import { formatDateTime } from '@vben/utils';
+
+import { Descriptions, Divider, Table, Tag } from 'ant-design-vue';
+
+import { getNotifyTaskDetail } from '#/api/pay/notify';
+import { DictTag } from '#/components/dict-tag';
+import { DICT_TYPE } from '#/utils/dict';
+
+import { detailColumns } from '../data';
+
+const formData = ref();
+
+const [Modal, modalApi] = useVbenModal({
+  async onOpenChange(isOpen: boolean) {
+    if (!isOpen) {
+      formData.value = undefined;
+      return;
+    }
+    // 加载数据
+    const data = modalApi.getData();
+    if (!data || !data.id) {
+      return;
+    }
+    modalApi.lock();
+    try {
+      formData.value = await getNotifyTaskDetail(data.id);
+    } finally {
+      modalApi.unlock();
+    }
+  },
+});
+
+/** 打开弹窗 */
+const open = (id: number) => {
+  modalApi.setData({ id }).open();
+};
+
+defineExpose({ open });
+</script>
+
+<template>
+  <Modal
+    title="通知详情"
+    class="w-1/2"
+    :show-cancel-button="false"
+    :show-confirm-button="false"
+  >
+    <Descriptions bordered :column="2" size="middle" class="mx-4">
+      <Descriptions.Item label="商户订单编号">
+        <Tag>{{ formData?.merchantOrderId }}</Tag>
+      </Descriptions.Item>
+      <Descriptions.Item label="通知状态">
+        <DictTag
+          :type="DICT_TYPE.PAY_NOTIFY_STATUS"
+          :value="formData?.status"
+        />
+      </Descriptions.Item>
+
+      <Descriptions.Item label="应用编号">
+        {{ formData?.appId }}
+      </Descriptions.Item>
+      <Descriptions.Item label="应用名称">
+        {{ formData?.appName }}
+      </Descriptions.Item>
+
+      <Descriptions.Item label="关联编号">
+        {{ formData?.dataId }}
+      </Descriptions.Item>
+      <Descriptions.Item label="通知类型">
+        <DictTag :type="DICT_TYPE.PAY_NOTIFY_TYPE" :value="formData?.type" />
+      </Descriptions.Item>
+
+      <Descriptions.Item label="通知次数">
+        {{ formData?.notifyTimes }}
+      </Descriptions.Item>
+      <Descriptions.Item label="最大通知次数">
+        {{ formData?.maxNotifyTimes }}
+      </Descriptions.Item>
+
+      <Descriptions.Item label="最后通知时间">
+        {{ formatDateTime(formData?.lastExecuteTime || '') }}
+      </Descriptions.Item>
+      <Descriptions.Item label="下次通知时间">
+        {{ formatDateTime(formData?.nextNotifyTime || '') }}
+      </Descriptions.Item>
+
+      <Descriptions.Item label="创建时间">
+        {{ formatDateTime(formData?.createTime || '') }}
+      </Descriptions.Item>
+      <Descriptions.Item label="更新时间">
+        {{ formatDateTime(formData?.updateTime || '') }}
+      </Descriptions.Item>
+    </Descriptions>
+
+    <Divider />
+
+    <Descriptions bordered :column="1" size="middle" class="mx-4">
+      <Descriptions.Item label="回调日志">
+        <Table :data="formData.logs" :columns="detailColumns" />
+      </Descriptions.Item>
+    </Descriptions>
+  </Modal>
+</template>
