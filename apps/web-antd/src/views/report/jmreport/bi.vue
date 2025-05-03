dcecde0967671b912526c9080d52da7cfd870eca
YunaiV <zhijiantianya@gmail.com>
2025-05-03 10:38:48 +0800

    feat：集成积木仪表盘

---
+++ apps/web-antd/src/views/report/jmreport/bi.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+import { Page } from '@vben/common-ui';
+import { useAccessStore } from '@vben/stores';
+
+import { DocAlert } from '#/components/doc-alert';
+import { IFrame } from '#/components/iframe';
+
+defineOptions({ name: 'JimuBI' });
+const accessStore = useAccessStore();
+
+const src = ref(
+  `${import.meta.env.VITE_BASE_URL}/drag/list?token=${
+    accessStore.refreshToken
+  }`,
+);
+</script>
+
+<template>
+  <Page auto-content-height>
+    <template #doc>
+      <DocAlert title="大屏设计器" url="https://doc.iocoder.cn/screen/" />
+    </template>
+
+    <IFrame :src="src" />
+  </Page>
+</template>
