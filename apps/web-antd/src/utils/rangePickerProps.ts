bcd913dd8dcc0b3ee84bf257bdfd317ade6ddb2f
xingyu4j <xingyu4j@vip.qq.com>
2025-05-06 23:38:20 +0800

    fix: getRangePickerDefaultProps

--- apps/web-antd/src/utils/rangePickerProps.ts
+++ apps/web-antd/src/utils/rangePickerProps.ts
@@ -1,3 +1,5 @@
+import type { Dayjs } from 'dayjs';
+
 import dayjs from 'dayjs';
 
 /** 时间段选择器拓展  */
@@ -6,21 +8,27 @@ export function getRangePickerDefaultProps() {
     format: 'YYYY-MM-DD HH:mm:ss',
     placeholder: ['开始时间', '结束时间'],
     ranges: {
-      今天: [dayjs().startOf('day'), dayjs().endOf('day')],
-      '最近 7 天': [
-        dayjs().subtract(7, 'day').startOf('day'),
-        dayjs().endOf('day'),
-      ],
-      '最近 30 天': [
-        dayjs().subtract(30, 'day').startOf('day'),
-        dayjs().endOf('day'),
-      ],
-      昨天: [
-        dayjs().subtract(1, 'day').startOf('day'),
-        dayjs().subtract(1, 'day').endOf('day'),
-      ],
-      本周: [dayjs().startOf('week'), dayjs().endOf('day')],
-      本月: [dayjs().startOf('month'), dayjs().endOf('day')],
+      今天: () =>
+        [dayjs().startOf('day'), dayjs().endOf('day')] as [Dayjs, Dayjs],
+      '最近 7 天': () =>
+        [dayjs().subtract(7, 'day').startOf('day'), dayjs().endOf('day')] as [
+          Dayjs,
+          Dayjs,
+        ],
+      '最近 30 天': () =>
+        [dayjs().subtract(30, 'day').startOf('day'), dayjs().endOf('day')] as [
+          Dayjs,
+          Dayjs,
+        ],
+      昨天: () =>
+        [
+          dayjs().subtract(1, 'day').startOf('day'),
+          dayjs().subtract(1, 'day').endOf('day'),
+        ] as [Dayjs, Dayjs],
+      本周: () =>
+        [dayjs().startOf('week'), dayjs().endOf('day')] as [Dayjs, Dayjs],
+      本月: () =>
+        [dayjs().startOf('month'), dayjs().endOf('day')] as [Dayjs, Dayjs],
     },
     showTime: {
       defaultValue: [
