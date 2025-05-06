34eebc14cfdb07b1e082feb7139ba456772b321b
ziye <278898052@qq.com>
2025-05-06 23:23:40 +0800

    feat: 完善流程监听器功能

---
+++ apps/web-antd/src/views/bpm/processListener/data.ts
@@ -0,0 +1,207 @@
+import type { VbenFormSchema } from '#/adapter/form';
+import type { OnActionClickFn, VxeTableGridOptions } from '#/adapter/vxe-table';
+import type { BpmCategoryApi } from '#/api/bpm/category';
+
+import { useAccess } from '@vben/access';
+
+import { z } from '#/adapter/form';
+import { CommonStatusEnum } from '#/utils/constants';
+import { DICT_TYPE, getDictOptions } from '#/utils/dict';
+
+export const EVENT_EXECUTION_OPTIONS = [
+  {
+    label: 'start',
+    value: 'start',
+  },
+  {
+    label: 'end',
+    value: 'end',
+  },
+];
+
+export const EVENT_OPTIONS = [
+  { label: 'create', value: 'create' },
+  { label: 'assignment', value: 'assignment' },
+  { label: 'complete', value: 'complete' },
+  { label: 'delete', value: 'delete' },
+  { label: 'update', value: 'update' },
+  { label: 'timeout', value: 'timeout' },
+];
+
+const { hasAccessByCodes } = useAccess();
+
+/** 新增/修改的表单 */
+export function useFormSchema(): VbenFormSchema[] {
+  return [
+    {
+      fieldName: 'id',
+      component: 'Input',
+      dependencies: {
+        triggerFields: [''],
+        show: () => false,
+      },
+    },
+    {
+      fieldName: 'name',
+      label: '名字',
+      component: 'Input',
+      componentProps: {
+        placeholder: '请输入名字',
+      },
+      rules: 'required',
+    },
+    {
+      fieldName: 'status',
+      label: '状态',
+      component: 'RadioGroup',
+      componentProps: {
+        options: getDictOptions(DICT_TYPE.COMMON_STATUS, 'number'),
+        placeholder: '请选择状态',
+        buttonStyle: 'solid',
+        optionType: 'button',
+      },
+      rules: z.number().default(CommonStatusEnum.ENABLE),
+    },
+    {
+      fieldName: 'type',
+      label: '类型',
+      component: 'Select',
+      componentProps: {
+        options: getDictOptions(DICT_TYPE.BPM_PROCESS_LISTENER_TYPE, 'string'),
+        allowClear: true,
+      },
+      rules: 'required',
+    },
+    {
+      fieldName: 'event',
+      label: '事件',
+      component: 'Select',
+      componentProps: {
+        options: EVENT_OPTIONS,
+        allowClear: true,
+      },
+      rules: 'required',
+    },
+    {
+      fieldName: 'valueType',
+      label: '值类型',
+      component: 'Select',
+      componentProps: {
+        options: getDictOptions(
+          DICT_TYPE.BPM_PROCESS_LISTENER_VALUE_TYPE,
+          'string',
+        ),
+        allowClear: true,
+      },
+      rules: 'required',
+    },
+    {
+      fieldName: 'value',
+      label: '表达式',
+      component: 'Input',
+      rules: 'required',
+    },
+  ];
+}
+
+/** 列表的搜索表单 */
+export function useGridFormSchema(): VbenFormSchema[] {
+  return [
+    {
+      fieldName: 'name',
+      label: '名字',
+      component: 'Input',
+      componentProps: {
+        placeholder: '请输入名字',
+        allowClear: true,
+      },
+    },
+    {
+      fieldName: 'type',
+      label: '类型',
+      component: 'Select',
+      componentProps: {
+        placeholder: '请选择类型',
+        options: getDictOptions(DICT_TYPE.BPM_PROCESS_LISTENER_TYPE, 'string'),
+        allowClear: true,
+      },
+    },
+  ];
+}
+
+/** 列表的字段 */
+export function useGridColumns<T = BpmCategoryApi.CategoryVO>(
+  onActionClick: OnActionClickFn<T>,
+): VxeTableGridOptions['columns'] {
+  return [
+    {
+      field: 'id',
+      title: '编号',
+      minWidth: 100,
+    },
+    {
+      field: 'name',
+      title: '名字',
+      minWidth: 200,
+    },
+    {
+      field: 'type',
+      title: '类型',
+      minWidth: 200,
+      cellRender: {
+        name: 'CellDict',
+        props: { type: DICT_TYPE.BPM_PROCESS_LISTENER_TYPE },
+      },
+    },
+    {
+      field: 'event',
+      title: '事件',
+      minWidth: 200,
+    },
+    {
+      field: 'valueType',
+      title: '值类型',
+      minWidth: 200,
+      cellRender: {
+        name: 'CellDict',
+        props: { type: DICT_TYPE.BPM_PROCESS_LISTENER_VALUE_TYPE },
+      },
+    },
+    {
+      field: 'value',
+      title: '值',
+      minWidth: 200,
+    },
+    {
+      field: 'createTime',
+      title: '创建时间',
+      minWidth: 180,
+      formatter: 'formatDateTime',
+    },
+    {
+      field: 'operation',
+      title: '操作',
+      minWidth: 180,
+      align: 'center',
+      fixed: 'right',
+      cellRender: {
+        attrs: {
+          nameField: 'name',
+          nameTitle: '流程监听器',
+          onClick: onActionClick,
+        },
+        name: 'CellOperation',
+        options: [
+          {
+            code: 'edit',
+            show: hasAccessByCodes(['bpm:process-listener:update']),
+          },
+          {
+            code: 'delete',
+            show: hasAccessByCodes(['bpm:process-listener:delete']),
+          },
+        ],
+      },
+    },
+  ];
+}
