59c593c1c165524b90e88abe99b4dd7d1eb8686a
xingyu4j <xingyu4j@vip.qq.com>
2025-05-09 16:15:40 +0800

    fix: type

--- apps/web-antd/src/components/cropper/index.ts
+++ apps/web-antd/src/components/cropper/index.ts
@@ -1,3 +1,3 @@
 export { default as CropperAvatar } from './cropper-avatar.vue';
 export { default as CropperImage } from './cropper.vue';
-export type { Cropper } from './typing';
+export type { CropperType } from './typing';
