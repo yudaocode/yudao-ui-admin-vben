30934894234d2b7706e14d9f081a79925b5f4e82
puhui999 <puhui999@163.com>
2025-05-12 00:00:28 +0800

    feat: 新增 ele OAuth 2.0 模块

---
+++ apps/web-ele/src/views/system/oauth2/client/index.vue
@@ -0,0 +1,131 @@
+<script lang="ts" setup>
+import type {
+  OnActionClickParams,
+  VxeTableGridOptions,
+} from '#/adapter/vxe-table';
+import type { SystemOAuth2ClientApi } from '#/api/system/oauth2/client';
+
+import { Page, useVbenModal } from '@vben/common-ui';
+import { Plus } from '@vben/icons';
+
+import { ElButton, ElLoading, ElMessage } from 'element-plus';
+
+import { useVbenVxeGrid } from '#/adapter/vxe-table';
+import {
+  deleteOAuth2Client,
+  getOAuth2ClientPage,
+} from '#/api/system/oauth2/client';
+import { DocAlert } from '#/components/doc-alert';
+import { $t } from '#/locales';
+
+import { useGridColumns, useGridFormSchema } from './data';
+import Form from './modules/form.vue';
+
+const [FormModal, formModalApi] = useVbenModal({
+  connectedComponent: Form,
+  destroyOnClose: true,
+});
+
+/** 刷新表格 */
+function onRefresh() {
+  gridApi.query();
+}
+
+/** 创建 OAuth2 客户端 */
+function onCreate() {
+  formModalApi.setData(null).open();
+}
+
+/** 编辑 OAuth2 客户端 */
+function onEdit(row: SystemOAuth2ClientApi.OAuth2Client) {
+  formModalApi.setData(row).open();
+}
+
+/** 删除 OAuth2 客户端 */
+async function onDelete(row: SystemOAuth2ClientApi.OAuth2Client) {
+  const loadingInstance = ElLoading.service({
+    text: $t('ui.actionMessage.deleting', [row.name]),
+    fullscreen: true,
+  });
+  try {
+    await deleteOAuth2Client(row.id as number);
+    ElMessage.success($t('ui.actionMessage.deleteSuccess', [row.name]));
+    onRefresh();
+  } catch {
+    // 异常处理
+  } finally {
+    loadingInstance.close();
+  }
+}
+
+/** 表格操作按钮的回调函数 */
+function onActionClick({
+  code,
+  row,
+}: OnActionClickParams<SystemOAuth2ClientApi.OAuth2Client>) {
+  switch (code) {
+    case 'delete': {
+      onDelete(row);
+      break;
+    }
+    case 'edit': {
+      onEdit(row);
+      break;
+    }
+  }
+}
+
+const [Grid, gridApi] = useVbenVxeGrid({
+  formOptions: {
+    schema: useGridFormSchema(),
+  },
+  gridOptions: {
+    columns: useGridColumns(onActionClick),
+    height: 'auto',
+    keepSource: true,
+    proxyConfig: {
+      ajax: {
+        query: async ({ page }, formValues) => {
+          return await getOAuth2ClientPage({
+            pageNo: page.currentPage,
+            pageSize: page.pageSize,
+            ...formValues,
+          });
+        },
+      },
+    },
+    rowConfig: {
+      keyField: 'id',
+    },
+    toolbarConfig: {
+      refresh: { code: 'query' },
+      search: true,
+    },
+  } as VxeTableGridOptions<SystemOAuth2ClientApi.OAuth2Client>,
+});
+</script>
+
+<template>
+  <Page auto-content-height>
+    <template #doc>
+      <DocAlert
+        title="OAuth 2.0（SSO 单点登录）"
+        url="https://doc.iocoder.cn/oauth2/"
+      />
+    </template>
+
+    <FormModal @success="onRefresh" />
+    <Grid table-title="OAuth2 客户端列表">
+      <template #toolbar-tools>
+        <ElButton
+          type="primary"
+          @click="onCreate"
+          v-access:code="['system:oauth2-client:create']"
+        >
+          <Plus class="size-5" />
+          {{ $t('ui.actionTitle.create', [' OAuth2.0 客户端']) }}
+        </ElButton>
+      </template>
+    </Grid>
+  </Page>
+</template>
