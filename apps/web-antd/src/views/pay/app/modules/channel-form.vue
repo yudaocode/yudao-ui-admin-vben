a602decde99dc3f8c195b85504af1012689c1bcd
xingyu4j <xingyu4j@vip.qq.com>
2025-05-26 16:52:11 +0800

    refactor: pay app

--- apps/web-antd/src/views/pay/app/modules/channel-form.vue
+++ apps/web-antd/src/views/pay/app/modules/channel-form.vue
@@ -1,104 +1,96 @@
 <script lang="ts" setup>
+import type { PayChannelApi } from '#/api/pay/channel';
+
 import { computed, ref } from 'vue';
 
 import { useVbenModal } from '@vben/common-ui';
 import { $t } from '@vben/locales';
-import { cloneDeep } from '@vben/utils';
 
-import { Row, Space, Textarea } from 'ant-design-vue';
+import { message, Row, Space, Textarea } from 'ant-design-vue';
 
 import { useVbenForm } from '#/adapter/form';
-import * as ChannelApi from '#/api/pay/channel';
+import { createChannel, getChannel, updateChannel } from '#/api/pay/channel';
 import { FileUpload } from '#/components/upload';
 
-import { modalAliPaySchema } from './data';
+import { channelSchema } from './data';
 
-const emit = defineEmits<{ reload: [] }>();
-
-const isUpdate = ref(false);
+const emit = defineEmits(['success']);
+const formData = ref<PayChannelApi.Channel>();
+const formType = ref<string>('');
 const title = computed(() => {
-  return isUpdate.value
+  return formData.value?.id
     ? $t('ui.actionTitle.edit', '应用')
     : $t('ui.actionTitle.create', '应用');
 });
 
-const [BasicForm, formApi] = useVbenForm({
+const [Form, formApi] = useVbenForm({
   commonConfig: {
-    // 默认占满两列
-    formItemClass: 'col-span-2',
-    // 默认label宽度 px
-    labelWidth: 160,
-    // 通用配置项 会影响到所有表单项
     componentProps: {
       class: 'w-full',
     },
+    formItemClass: 'col-span-2',
+    labelWidth: 160,
   },
-  schema: modalAliPaySchema(),
+  layout: 'horizontal',
   showDefaultActions: false,
-  wrapperClass: 'grid-cols-2',
 });
 
-const [BasicModal, modalApi] = useVbenModal({
-  fullscreenButton: false,
-  onCancel: handleCancel,
-  onConfirm: handleConfirm,
-  onOpenChange: async (isOpen) => {
-    if (!isOpen) {
-      return null;
-    }
-    modalApi.modalLoading(true);
-
-    const { id, payCode } = modalApi.getData() as {
-      id?: number;
-      payCode?: string;
-    };
-
-    if (id && payCode) {
-      const record = await ChannelApi.getChannel(id, payCode);
-      isUpdate.value = !!record;
-      record.code = payCode;
-      if (isUpdate.value) {
-        record.config = JSON.parse(record.config);
-        await formApi.setValues(record);
-      }
-    }
-
-    modalApi.modalLoading(false);
-  },
-});
-
-async function handleConfirm() {
-  try {
-    modalApi.modalLoading(true);
+const [Modal, modalApi] = useVbenModal({
+  async onConfirm() {
     const { valid } = await formApi.validate();
     if (!valid) {
       return;
     }
-    // getValues获取为一个readonly的对象 需要修改必须先深拷贝一次
-    const data = cloneDeep(
-      await formApi.getValues(),
-    ) as ChannelApi.PayChannelApi.Channel;
-    data.config = JSON.stringify(data.config);
-    await (isUpdate.value
-      ? ChannelApi.updateChannel(data)
-      : ChannelApi.createChannel(data));
-    emit('reload');
-    await handleCancel();
-  } catch (error) {
-    console.error(error);
-  } finally {
-    modalApi.modalLoading(false);
-  }
-}
+    modalApi.lock();
+    // 提交表单
+    const data = (await formApi.getValues()) as PayChannelApi.Channel;
+    try {
+      await (formData.value?.id ? updateChannel(data) : createChannel(data));
+      // 关闭并提示
+      await modalApi.close();
+      emit('success');
+      message.success($t('ui.actionMessage.operationSuccess'));
+    } finally {
+      modalApi.unlock();
+    }
+  },
+  onOpenChange: async (isOpen) => {
+    if (!isOpen) {
+      formData.value = undefined;
+      return;
+    }
+    // 加载数据
+    const { id, payCode } = modalApi.getData() as {
+      id?: number;
+      payCode?: string;
+    };
+    if (!id || !payCode) {
+      return;
+    }
+    modalApi.lock();
+    if (payCode.includes('alipay_')) {
+      formType.value = 'alipay';
+    } else if (payCode.includes('mock')) {
+      formType.value = 'mock';
+    } else if (payCode.includes('wallet')) {
+      formType.value = 'wallet';
+    } else if (payCode.includes('wx')) {
+      formType.value = 'wx';
+    }
 
-async function handleCancel() {
-  modalApi.close();
-  await formApi.resetForm();
-}
+    try {
+      formData.value = await getChannel(id, payCode);
+      // 设置到 values
+      await formApi.setValues(formData.value);
+    } finally {
+      modalApi.unlock();
+    }
+  },
+});
 </script>
 <template>
-  <BasicModal :close-on-click-modal="false" :title="title" class="w-[40%]">
-    <BasicForm>
+  <Modal :close-on-click-modal="false" :title="title" class="w-[40%]">
+    <Form :schema="channelSchema(formType)">
       <template #appCertContent="slotProps">
         <Space style="width: 100%" direction="vertical">
           <Row>
@@ -158,6 +150,6 @@ async function handleCancel() {
           </Row>
         </Space>
       </template>
-    </BasicForm>
-  </BasicModal>
+    </Form>
+  </Modal>
 </template>
