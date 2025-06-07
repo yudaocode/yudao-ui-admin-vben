1b3e2eef812b6f5e55c40eb2cc8b8fadbc6af4d7
xingyu4j <xingyu4j@vip.qq.com>
2025-06-07 11:53:32 +0800

    feat: mall comment

--- apps/web-antd/src/api/mall/product/comment.ts
+++ apps/web-antd/src/api/mall/product/comment.ts
@@ -3,6 +3,12 @@ import type { PageParam, PageResult } from '@vben/request';
 import { requestClient } from '#/api/request';
 
 export namespace MallCommentApi {
+  export interface Property {
+    propertyId: number;
+    propertyName: string;
+    valueId: number;
+    valueName: string;
+  }
   /** 商品评论 */
   export interface Comment {
     id: number;
@@ -20,11 +26,13 @@ export namespace MallCommentApi {
     descriptionScores: number;
     benefitScores: number;
     content: string;
-    picUrls: string;
+    picUrls: string[];
     replyStatus: boolean;
     replyUserId: number;
     replyContent: string;
     replyTime: Date;
+    createTime: Date;
+    skuProperties: Property[];
   }
 
   /** 评论可见性更新 */
