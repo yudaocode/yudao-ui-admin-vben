065dde94e856505b7236062fbb9a2b2fcc957b7a
xingyu4j <xingyu4j@vip.qq.com>
2025-06-20 14:03:10 +0800

    feat: mall combination

--- apps/web-antd/src/views/mall/promotion/combination/record/index.vue
+++ apps/web-antd/src/views/mall/promotion/combination/record/index.vue
@@ -1,32 +1,81 @@
 <script lang="ts" setup>
-import { DocAlert, Page } from '@vben/common-ui';
+import type { VxeTableGridOptions } from '#/adapter/vxe-table';
 
-import { Button } from 'ant-design-vue';
+import { DocAlert, Page, useVbenModal } from '@vben/common-ui';
+
+import { ACTION_ICON, TableAction, useVbenVxeGrid } from '#/adapter/vxe-table';
+import { getCombinationRecordPage } from '#/api/mall/promotion/combination/combinationRecord';
+
+import { useGridColumns, useGridFormSchema } from './data';
+import CombinationUserList from './modules/list.vue';
+
+defineOptions({ name: 'PromotionCombinationRecord' });
+
+const [UserListModal, userListModalApi] = useVbenModal({
+  connectedComponent: CombinationUserList,
+  destroyOnClose: true,
+});
+
+/** 查看拼团用户 */
+function handleViewUsers(row: any) {
+  userListModalApi.setData({ recordId: row.id }).open();
+}
+
+const [Grid] = useVbenVxeGrid({
+  formOptions: {
+    schema: useGridFormSchema(),
+  },
+  gridOptions: {
+    columns: useGridColumns(),
+    height: 'auto',
+    keepSource: true,
+    proxyConfig: {
+      ajax: {
+        query: async ({ page }, formValues) => {
+          return await getCombinationRecordPage({
+            pageNo: page.currentPage,
+            pageSize: page.pageSize,
+            ...formValues,
+          });
+        },
+      },
+    },
+    rowConfig: {
+      keyField: 'id',
+      isHover: true,
+    },
+    toolbarConfig: {
+      refresh: { code: 'query' },
+      search: true,
+    },
+  } as VxeTableGridOptions,
+});
 </script>
 
 <template>
-  <Page>
-    <DocAlert
-      title="【营销】拼团活动"
-      url="https://doc.iocoder.cn/mall/promotion-combination/"
-    />
-    <Button
-      danger
-      type="link"
-      target="_blank"
-      href="https://github.com/yudaocode/yudao-ui-admin-vue3"
-    >
-      该功能支持 Vue3 + element-plus 版本！
-    </Button>
-    <br />
-    <Button
-      type="link"
-      target="_blank"
-      href="https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/mall/promotion/combination/record/index.vue"
-    >
-      可参考
-      https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/mall/promotion/combination/record/index.vue
-      代码，pull request 贡献给我们！
-    </Button>
+  <Page auto-content-height>
+    <template #doc>
+      <DocAlert
+        title="【营销】拼团活动"
+        url="https://doc.iocoder.cn/mall/promotion-combination/"
+      />
+    </template>
+
+    <UserListModal />
+
+    <Grid table-title="拼团记录列表">
+      <template #actions="{ row }">
+        <TableAction
+          :actions="[
+            {
+              label: '查看成员',
+              type: 'link',
+              icon: ACTION_ICON.VIEW,
+              onClick: handleViewUsers.bind(null, row),
+            },
+          ]"
+        />
+      </template>
+    </Grid>
   </Page>
 </template>
