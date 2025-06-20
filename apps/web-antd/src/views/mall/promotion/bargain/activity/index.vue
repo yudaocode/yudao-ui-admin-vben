8ff0f216a989281fe81e8ba59672bfbdb8430079
xingyu4j <xingyu4j@vip.qq.com>
2025-06-20 11:16:42 +0800

    feat: mall bargain activity

--- apps/web-antd/src/views/mall/promotion/bargain/activity/index.vue
+++ apps/web-antd/src/views/mall/promotion/bargain/activity/index.vue
@@ -1,32 +1,178 @@
 <script lang="ts" setup>
-import { DocAlert, Page } from '@vben/common-ui';
+import type { VxeTableGridOptions } from '#/adapter/vxe-table';
+import type { MallBargainActivityApi } from '#/api/mall/promotion/bargain/bargainActivity';
 
-import { Button } from 'ant-design-vue';
+import { confirm, DocAlert, Page, useVbenModal } from '@vben/common-ui';
+
+import { message } from 'ant-design-vue';
+
+import { ACTION_ICON, TableAction, useVbenVxeGrid } from '#/adapter/vxe-table';
+import {
+  closeBargainActivity,
+  deleteBargainActivity,
+  getBargainActivityPage,
+} from '#/api/mall/promotion/bargain/bargainActivity';
+import { $t } from '#/locales';
+
+import { useGridColumns, useGridFormSchema } from './data';
+import Form from './modules/form.vue';
+
+defineOptions({ name: 'PromotionBargainActivity' });
+
+const [FormModal, formModalApi] = useVbenModal({
+  connectedComponent: Form,
+  destroyOnClose: true,
+});
+
+/** 刷新表格 */
+function onRefresh() {
+  gridApi.query();
+}
+
+/** 创建砍价活动 */
+function handleCreate() {
+  formModalApi.setData(null).open();
+}
+
+/** 编辑砍价活动 */
+function handleEdit(row: MallBargainActivityApi.BargainActivity) {
+  formModalApi.setData(row).open();
+}
+
+/** 关闭砍价活动 */
+async function handleClose(row: MallBargainActivityApi.BargainActivity) {
+  try {
+    await confirm({
+      content: '确认关闭该砍价活动吗？',
+    });
+  } catch {
+    return;
+  }
+
+  const hideLoading = message.loading({
+    content: '确认关闭该砍价活动吗？',
+    key: 'action_key_msg',
+  });
+  try {
+    await closeBargainActivity(row.id as number);
+    message.success({
+      content: '关闭成功',
+      key: 'action_key_msg',
+    });
+    onRefresh();
+  } finally {
+    hideLoading();
+  }
+}
+
+/** 删除砍价活动 */
+async function handleDelete(row: MallBargainActivityApi.BargainActivity) {
+  const hideLoading = message.loading({
+    content: $t('ui.actionMessage.deleting', [row.name]),
+    key: 'action_key_msg',
+  });
+  try {
+    await deleteBargainActivity(row.id as number);
+    message.success({
+      content: $t('ui.actionMessage.deleteSuccess', [row.name]),
+      key: 'action_key_msg',
+    });
+    onRefresh();
+  } finally {
+    hideLoading();
+  }
+}
+
+const [Grid, gridApi] = useVbenVxeGrid({
+  formOptions: {
+    schema: useGridFormSchema(),
+  },
+  gridOptions: {
+    columns: useGridColumns(),
+    height: 'auto',
+    keepSource: true,
+    proxyConfig: {
+      ajax: {
+        query: async ({ page }, formValues) => {
+          return await getBargainActivityPage({
+            pageNo: page.currentPage,
+            pageSize: page.pageSize,
+            ...formValues,
+          });
+        },
+      },
+    },
+    rowConfig: {
+      keyField: 'id',
+      isHover: true,
+    },
+    toolbarConfig: {
+      refresh: { code: 'query' },
+      search: true,
+    },
+  } as VxeTableGridOptions<MallBargainActivityApi.BargainActivity>,
+});
 </script>
 
 <template>
-  <Page>
-    <DocAlert
-      title="【营销】砍价活动"
-      url="https://doc.iocoder.cn/mall/promotion-bargain/"
-    />
-    <Button
-      danger
-      type="link"
-      target="_blank"
-      href="https://github.com/yudaocode/yudao-ui-admin-vue3"
-    >
-      该功能支持 Vue3 + element-plus 版本！
-    </Button>
-    <br />
-    <Button
-      type="link"
-      target="_blank"
-      href="https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/mall/promotion/bargain/activity/index"
-    >
-      可参考
-      https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/mall/promotion/bargain/activity/index
-      代码，pull request 贡献给我们！
-    </Button>
+  <Page auto-content-height>
+    <template #doc>
+      <DocAlert
+        title="【营销】砍价活动"
+        url="https://doc.iocoder.cn/mall/promotion-bargain/"
+      />
+    </template>
+
+    <FormModal @success="onRefresh" />
+
+    <Grid table-title="砍价活动列表">
+      <template #toolbar-tools>
+        <TableAction
+          :actions="[
+            {
+              label: $t('ui.actionTitle.create', ['砍价活动']),
+              type: 'primary',
+              icon: ACTION_ICON.ADD,
+              auth: ['promotion:bargain-activity:create'],
+              onClick: handleCreate,
+            },
+          ]"
+        />
+      </template>
+      <template #actions="{ row }">
+        <TableAction
+          :actions="[
+            {
+              label: $t('common.edit'),
+              type: 'link',
+              icon: ACTION_ICON.EDIT,
+              auth: ['promotion:bargain-activity:update'],
+              onClick: handleEdit.bind(null, row),
+            },
+            {
+              label: '关闭',
+              type: 'link',
+              danger: true,
+              icon: ACTION_ICON.DELETE,
+              auth: ['promotion:bargain-activity:close'],
+              ifShow: row.status === 0,
+              onClick: handleClose.bind(null, row),
+            },
+            {
+              label: $t('common.delete'),
+              type: 'link',
+              danger: true,
+              icon: ACTION_ICON.DELETE,
+              auth: ['promotion:bargain-activity:delete'],
+              ifShow: row.status !== 0,
+              popConfirm: {
+                title: $t('ui.actionMessage.deleteConfirm', [row.name]),
+                confirm: handleDelete.bind(null, row),
+              },
+            },
+          ]"
+        />
+      </template>
+    </Grid>
   </Page>
 </template>
