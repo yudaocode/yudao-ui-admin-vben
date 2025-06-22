f5f28380dba9820b767497a16197c6e10d85ea2f
xingyu4j <xingyu4j@vip.qq.com>
2025-06-22 16:22:44 +0800

    fix: help style

--- packages/effects/layouts/src/widgets/help/help.vue
+++ packages/effects/layouts/src/widgets/help/help.vue
@@ -29,7 +29,6 @@ const [Modal, modalApi] = useVbenModal({
   <Modal class="w-1/3" :title="$t('ui.widgets.qa')">
     <div class="mt-2 flex flex-col">
       <div class="mt-2 flex flex-col">
-        <!-- TODO @xingyu：要不要垂直？1. 项目地址；2. 问题反馈；3. 开发文档 -->
         <VbenButtonGroup class="basis-1/3" :gap="2" border size="large">
           <p class="w-24 p-2">项目地址:</p>
           <VbenButton
