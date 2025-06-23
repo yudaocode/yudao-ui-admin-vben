62655e23e55fa0176f52e0119f464160417049f9
xingyu4j <xingyu4j@vip.qq.com>
2025-06-23 21:14:48 +0800

    feat: crm statistics performance

--- apps/web-antd/src/views/crm/statistics/performance/index.vue
+++ apps/web-antd/src/views/crm/statistics/performance/index.vue
@@ -1,28 +1,156 @@
 <script lang="ts" setup>
-import { Page } from '@vben/common-ui';
+import type { EchartsUIType } from '@vben/plugins/echarts';
 
-import { Button } from 'ant-design-vue';
+import type { VxeTableGridOptions } from '#/adapter/vxe-table';
+import type { CrmStatisticsCustomerApi } from '#/api/crm/statistics/customer';
+
+import { onMounted, ref } from 'vue';
+
+import { Page } from '@vben/common-ui';
+import { EchartsUI, useEcharts } from '@vben/plugins/echarts';
+
+import { Tabs } from 'ant-design-vue';
+
+import { useVbenVxeGrid } from '#/adapter/vxe-table';
+import {
+  getContractCountPerformance,
+  getContractPricePerformance,
+  getReceivablePricePerformance,
+} from '#/api/crm/statistics/performance';
+
+import { getChartOptions } from './chartOptions';
+import { customerSummaryTabs, useGridFormSchema } from './data';
+
+const activeTabName = ref('ContractCountPerformance');
+const chartRef = ref<EchartsUIType>();
+const { renderEcharts } = useEcharts(chartRef);
+
+const [Grid, gridApi] = useVbenVxeGrid({
+  formOptions: {
+    schema: useGridFormSchema(),
+    handleSubmit: async () => {
+      await handleTabChange(activeTabName.value);
+    },
+  },
+  gridOptions: {
+    columns: [],
+    height: 'auto',
+    keepSource: true,
+    pagerConfig: {
+      enabled: false,
+    },
+    proxyConfig: {
+      enabled: false,
+    },
+    data: [],
+    rowConfig: {
+      keyField: 'id',
+      isHover: true,
+    },
+    toolbarConfig: {
+      enabled: false,
+    },
+  } as VxeTableGridOptions<CrmStatisticsCustomerApi.CustomerSummaryByUser>,
+});
+
+async function handleTabChange(key: any) {
+  activeTabName.value = key;
+  const params = (await gridApi.formApi.getValues()) as any;
+  let data: any[] = [];
+  const columnsData: any[] = [];
+  let tableData: any[] = [];
+  switch (key) {
+    case 'ContractCountPerformance': {
+      tableData = [
+        { title: '当月合同数量统计（个）' },
+        { title: '上月合同数量统计（个）' },
+        { title: '去年当月合同数量统计（个）' },
+        { title: '环比增长率（%）' },
+        { title: '同比增长率（%）' },
+      ];
+      data = await getContractCountPerformance(params);
+      break;
+    }
+    case 'ContractPricePerformance': {
+      tableData = [
+        { title: '当月合同金额统计（元）' },
+        { title: '上月合同金额统计（元）' },
+        { title: '去年当月合同金额统计（元）' },
+        { title: '环比增长率（%）' },
+        { title: '同比增长率（%）' },
+      ];
+      data = await getContractPricePerformance(params);
+      break;
+    }
+    case 'ReceivablePricePerformance': {
+      tableData = [
+        { title: '当月回款金额统计（元）' },
+        { title: '上月回款金额统计（元）' },
+        { title: '去年当月回款金额统计（元）' },
+        { title: '环比增长率（%）' },
+        { title: '同比增长率（%）' },
+      ];
+      data = await getReceivablePricePerformance(params);
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+  const columnObj = {
+    title: '日期',
+    field: 'title',
+    minWidth: 200,
+    align: 'left',
+  };
+  columnsData.splice(0); // 清空数组
+  columnsData.push(columnObj);
+  data.forEach((item: any, index: number) => {
+    const columnObj = { title: item.time, field: `field${index}` };
+    columnsData.push(columnObj);
+    tableData[0][`field${index}`] = item.currentMonthCount;
+    tableData[1][`field${index}`] = item.lastMonthCount;
+    tableData[2][`field${index}`] = item.lastYearCount;
+    tableData[3][`field${index}`] =
+      item.lastMonthCount === 0
+        ? 'NULL'
+        : (
+            ((item.currentMonthCount - item.lastMonthCount) /
+              item.lastMonthCount) *
+            100
+          ).toFixed(2);
+    tableData[4][`field${index}`] =
+      item.lastYearCount === 0
+        ? 'NULL'
+        : (
+            ((item.currentMonthCount - item.lastYearCount) /
+              item.lastYearCount) *
+            100
+          ).toFixed(2);
+  });
+  renderEcharts(getChartOptions(key, data), true);
+  gridApi.grid.reloadColumn(columnsData);
+  gridApi.grid.reloadData(tableData);
+}
+onMounted(() => {
+  handleTabChange(activeTabName.value);
+});
 </script>
 
 <template>
-  <Page>
-    <Button
-      danger
-      type="link"
-      target="_blank"
-      href="https://github.com/yudaocode/yudao-ui-admin-vue3"
-    >
-      该功能支持 Vue3 + element-plus 版本！
-    </Button>
-    <br />
-    <Button
-      type="link"
-      target="_blank"
-      href="https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/crm/statistics/performance/index"
-    >
-      可参考
-      https://github.com/yudaocode/yudao-ui-admin-vue3/blob/master/src/views/crm/statistics/performance/index
-      代码，pull request 贡献给我们！
-    </Button>
+  <Page auto-content-height>
+    <Grid>
+      <template #top>
+        <Tabs v-model:active-key="activeTabName" @change="handleTabChange">
+          <Tabs.TabPane
+            v-for="item in customerSummaryTabs"
+            :key="item.key"
+            :tab="item.tab"
+            :force-render="true"
+          />
+        </Tabs>
+        <EchartsUI class="mb-20 h-full w-full" ref="chartRef" />
+      </template>
+    </Grid>
   </Page>
 </template>
