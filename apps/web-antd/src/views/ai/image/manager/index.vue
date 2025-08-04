990ae4463aa2b2a2e499eb9139fb23d6ab300b15
xingyu4j <xingyu4j@vip.qq.com>
2025-08-04 18:43:33 +0800

    fix: types

--- apps/web-antd/src/views/ai/image/manager/index.vue
+++ apps/web-antd/src/views/ai/image/manager/index.vue
@@ -104,7 +104,10 @@ onMounted(async () => {
       </template>
       <template #userId="{ row }">
         <span>
-          {{ userList.find((item) => item.id === row.userId)?.nickname }}
+          {{
+            userList.find((item: SystemUserApi.User) => item.id === row.userId)
+              ?.nickname
+          }}
         </span>
       </template>
       <template #publicStatus="{ row }">
