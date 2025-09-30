05ec6a16b3dd8757313cf75e9c49bc11b24a0aa9
YunaiV <zhijiantianya@gmail.com>
2025-09-30 09:24:52 +0800

    feat：【antd】【crm】优化 receivable-plan 的整体代码风格

--- apps/web-antd/src/views/crm/receivable/plan/detail/index.vue
+++ apps/web-antd/src/views/crm/receivable/plan/detail/index.vue
@@ -1,43 +1,43 @@
 <script setup lang="ts">
-import type { CrmReceivablePlanApi } from '#/api/crm/receivable/plan';
-import type { SystemOperateLogApi } from '#/api/system/operate-log';
+import type {CrmReceivablePlanApi} from '#/api/crm/receivable/plan';
+import {getReceivablePlan} from '#/api/crm/receivable/plan';
+import type {SystemOperateLogApi} from '#/api/system/operate-log';
 
-import { computed, onMounted, ref } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
+import {onMounted, ref} from 'vue';
+import {useRoute, useRouter} from 'vue-router';
 
-import { Page, useVbenModal } from '@vben/common-ui';
-import { useTabs } from '@vben/hooks';
-import { ArrowLeft } from '@vben/icons';
+import {Page, useVbenModal} from '@vben/common-ui';
+import {useTabs} from '@vben/hooks';
 
-import { Button, Card, Tabs } from 'ant-design-vue';
+import { Card, Tabs} from 'ant-design-vue';
 
-import { getOperateLogPage } from '#/api/crm/operateLog';
-import { BizTypeEnum } from '#/api/crm/permission';
-import { getReceivablePlan } from '#/api/crm/receivable/plan';
-import { useDescription } from '#/components/description';
-import { AsyncOperateLog } from '#/components/operate-log';
-import { PermissionList } from '#/views/crm/permission';
-import { ReceivablePlanDetailsInfo } from '#/views/crm/receivable';
+import {ACTION_ICON, TableAction} from '#/adapter/vxe-table';
 
-import { useDetailSchema } from './data';
+import {getOperateLogPage} from '#/api/crm/operateLog';
+import {BizTypeEnum} from '#/api/crm/permission';
+import {useDescription} from '#/components/description';
+import {AsyncOperateLog} from '#/components/operate-log';
+import {PermissionList} from '#/views/crm/permission';
+import { ReceivablePlanDetailsInfo} from '#/views/crm/receivable';
+import { $t } from '#/locales';
+
+import {useDetailSchema} from './data';
 import ReceivablePlanForm from '../modules/form.vue';
-
-const loading = ref(false);
 
 const route = useRoute();
 const router = useRouter();
 const tabs = useTabs();
 
-const receivablePlanId = ref(0);
-
+const loading = ref(false); // 加载中
+const receivablePlanId = ref(0); // 回款计划编号
 const receivablePlan = ref<CrmReceivablePlanApi.Plan>(
   {} as CrmReceivablePlanApi.Plan,
 );
-const receivablePlanLogList = ref<SystemOperateLogApi.OperateLog[]>([]);
+const logList = ref<SystemOperateLogApi.OperateLog[]>([]); // 操作日志
 const permissionListRef = ref<InstanceType<typeof PermissionList>>(); // 团队成员列表 Ref
 
 // 校验编辑权限
-const validateWrite = computed(() => permissionListRef.value?.validateWrite);
+const validateWrite = () => permissionListRef.value?.validateWrite;
 
 const [Descriptions] = useDescription({
   componentProps: {
@@ -54,17 +54,19 @@ const [FormModal, formModalApi] = useVbenModal({
 });
 
 /** 加载回款计划详情 */
-async function loadreceivablePlanDetail() {
+async function getReceivablePlanDetail() {
   loading.value = true;
-  const data = await getReceivablePlan(receivablePlanId.value);
-  receivablePlan.value = data;
-  // 操作日志
-  const logList = await getOperateLogPage({
-    bizType: BizTypeEnum.CRM_RECEIVABLE_PLAN,
-    bizId: receivablePlanId.value,
-  });
-  receivablePlanLogList.value = logList.list;
-  loading.value = false;
+  try {
+    receivablePlan.value = await getReceivablePlan(receivablePlanId.value);
+    // 操作日志
+    const res = await getOperateLogPage({
+      bizType: BizTypeEnum.CRM_RECEIVABLE_PLAN,
+      bizId: receivablePlanId.value,
+    });
+    logList.value = res.list;
+  } finally {
+    loading.value = false;
+  }
 }
 
 /** 返回列表页 */
@@ -78,35 +80,39 @@ function handleEdit() {
   formModalApi.setData({ id: receivablePlanId.value }).open();
 }
 
-// 加载数据
+/** 加载数据 */
 onMounted(() => {
   receivablePlanId.value = Number(route.params.id);
-  loadreceivablePlanDetail();
+  getReceivablePlanDetail();
 });
 </script>
 
 <template>
   <Page
     auto-content-height
-    :title="`第${receivablePlan?.period}期`"
+    :title="`第 ${receivablePlan?.period} 期`"
     :loading="loading"
   >
-    <FormModal @success="loadreceivablePlanDetail" />
+    <FormModal @success="getReceivablePlanDetail" />
     <template #extra>
-      <div class="flex items-center gap-2">
-        <Button @click="handleBack">
-          <ArrowLeft class="size-5" />
-          返回
-        </Button>
-        <Button
-          v-if="validateWrite"
-          type="primary"
-          @click="handleEdit"
-          v-access:code="['crm:receivable-plan:update']"
-        >
-          {{ $t('ui.actionTitle.edit') }}
-        </Button>
-      </div>
+      <TableAction
+        :actions="[
+          {
+            label: '返回',
+            type: 'default',
+            icon: 'lucide:arrow-left',
+            onClick: handleBack,
+          },
+          {
+            label: $t('ui.actionTitle.edit'),
+            type: 'primary',
+            icon: ACTION_ICON.EDIT,
+            disabled: !validateWrite(),
+            onClick: handleEdit,
+            auth: ['crm:receivable-plan:update'],
+          },
+        ]"
+      />
     </template>
     <Card class="min-h-[10%]">
       <Descriptions :data="receivablePlan" />
@@ -126,7 +132,7 @@ onMounted(() => {
           />
         </Tabs.TabPane>
         <Tabs.TabPane tab="操作日志" key="3" :force-render="true">
-          <AsyncOperateLog :log-list="receivablePlanLogList" />
+          <AsyncOperateLog :log-list="logList" />
         </Tabs.TabPane>
       </Tabs>
     </Card>
