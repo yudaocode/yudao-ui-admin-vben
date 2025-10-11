2f0ba0458eec9ae8ad4f75ad1d73fd514d51faa1
YunaiV <zhijiantianya@gmail.com>
2025-10-11 13:06:22 +0800

    feat：【mall 商城】分销提现的迁移（antd 100%）样式优化

--- apps/web-antd/src/views/mall/trade/brokerage/withdraw/index.vue
+++ apps/web-antd/src/views/mall/trade/brokerage/withdraw/index.vue
@@ -100,7 +100,7 @@ const [Grid, gridApi] = useVbenVxeGrid({
     height: 'auto',
     keepSource: true,
     cellConfig: {
-      height: 80,
+      height: 90,
     },
     proxyConfig: {
       ajax: {
@@ -129,35 +129,57 @@ const [Grid, gridApi] = useVbenVxeGrid({
   <Page auto-content-height>
     <Grid table-title="佣金提现列表">
       <template #withdraw-info="{ row }">
-        <div v-if="row.type === BrokerageWithdrawTypeEnum.WALLET.type">-</div>
-        <div v-else>
-          <div v-if="row.userAccount">账号：{{ row.userAccount }}</div>
-          <div v-if="row.userName">真实姓名：{{ row.userName }}</div>
+        <div
+          v-if="row.type === BrokerageWithdrawTypeEnum.WALLET.type"
+          class="text-left"
+        >
+          -
+        </div>
+        <div v-else class="text-left">
+          <div v-if="row.userAccount" class="text-left">
+            账号：{{ row.userAccount }}
+          </div>
+          <div v-if="row.userName" class="text-left">
+            真实姓名：{{ row.userName }}
+          </div>
           <template v-if="row.type === BrokerageWithdrawTypeEnum.BANK.type">
-            <div v-if="row.bankName">银行名称：{{ row.bankName }}</div>
-            <div v-if="row.bankAddress">开户地址：{{ row.bankAddress }}</div>
+            <div v-if="row.bankName" class="text-left">
+              银行名称：{{ row.bankName }}
+            </div>
+            <div v-if="row.bankAddress" class="text-left">
+              开户地址：{{ row.bankAddress }}
+            </div>
           </template>
-          <div v-if="row.qrCodeUrl" class="mt-2">
-            <div>收款码：</div>
-            <div>
-              <img :src="row.qrCodeUrl" class="mt-1 h-10 w-10" />
+          <div v-if="row.qrCodeUrl" class="mt-2 text-left">
+            <div class="flex items-start gap-2">
+              <span class="flex-shrink-0">收款码：</span>
+              <img :src="row.qrCodeUrl" class="h-10 w-10 flex-shrink-0" />
             </div>
           </div>
         </div>
       </template>
       <template #status-info="{ row }">
-        <div>
+        <div class="text-left">
           <DictTag
             :value="row.status"
             :type="DICT_TYPE.BROKERAGE_WITHDRAW_STATUS"
           />
-          <div v-if="row.auditTime" class="mt-1 text-xs text-gray-500">
+          <div
+            v-if="row.auditTime"
+            class="mt-1 text-left text-xs text-gray-500"
+          >
             时间：{{ formatDateTime(row.auditTime) }}
           </div>
-          <div v-if="row.auditReason" class="mt-1 text-xs text-gray-500">
+          <div
+            v-if="row.auditReason"
+            class="mt-1 text-left text-xs text-gray-500"
+          >
             审核原因：{{ row.auditReason }}
           </div>
-          <div v-if="row.transferErrorMsg" class="mt-1 text-xs text-red-500">
+          <div
+            v-if="row.transferErrorMsg"
+            class="mt-1 text-left text-xs text-red-500"
+          >
             转账失败原因：{{ row.transferErrorMsg }}
           </div>
         </div>
