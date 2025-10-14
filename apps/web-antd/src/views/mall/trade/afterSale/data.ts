6fe61f9efbb4a790791ac91c5345640ca76604f1
YunaiV <zhijiantianya@gmail.com>
2025-10-14 20:52:35 +0800

    feat：【mall 商城】售后退款（100% antd table）

--- apps/web-antd/src/views/mall/trade/afterSale/data.ts
+++ apps/web-antd/src/views/mall/trade/afterSale/data.ts
@@ -7,60 +7,6 @@ import { getDictOptions } from '@vben/hooks';
 import { z } from '#/adapter/form';
 import { getRangePickerDefaultProps } from '#/utils';
 
-/** 列表的搜索表单 */
-export function useGridFormSchema(): VbenFormSchema[] {
-  return [
-    {
-      fieldName: 'spuName',
-      label: '商品名称',
-      component: 'Input',
-    },
-    {
-      fieldName: 'no',
-      label: '退款编号',
-      component: 'Input',
-    },
-    {
-      fieldName: 'orderNo',
-      label: '订单编号',
-      component: 'Input',
-    },
-    {
-      fieldName: 'status',
-      label: '售后状态',
-      component: 'Select',
-      componentProps: {
-        options: getDictOptions(DICT_TYPE.TRADE_AFTER_SALE_STATUS, 'number'),
-      },
-    },
-    {
-      fieldName: 'status',
-      label: '售后方式',
-      component: 'Select',
-      componentProps: {
-        options: getDictOptions(DICT_TYPE.TRADE_AFTER_SALE_WAY, 'number'),
-      },
-    },
-    {
-      fieldName: 'type',
-      label: '售后类型',
-      component: 'Select',
-      componentProps: {
-        options: getDictOptions(DICT_TYPE.TRADE_AFTER_SALE_TYPE, 'number'),
-      },
-    },
-    {
-      fieldName: 'createTime',
-      label: '创建时间',
-      component: 'RangePicker',
-      componentProps: {
-        ...getRangePickerDefaultProps(),
-        allowClear: true,
-      },
-    },
-  ];
-}
-
 /** 拒绝售后表单的 schema 配置 */
 export function useDisagreeFormSchema(): VbenFormSchema[] {
   return [
@@ -85,6 +31,75 @@ export function useDisagreeFormSchema(): VbenFormSchema[] {
   ];
 }
 
+/** 列表的搜索表单 */
+export function useGridFormSchema(): VbenFormSchema[] {
+  return [
+    {
+      fieldName: 'spuName',
+      label: '商品名称',
+      component: 'Input',
+      componentProps: {
+        placeholder: '请输入商品名称',
+      },
+    },
+    {
+      fieldName: 'no',
+      label: '退款编号',
+      component: 'Input',
+      componentProps: {
+        placeholder: '请输入退款编号',
+      },
+    },
+    {
+      fieldName: 'orderNo',
+      label: '订单编号',
+      component: 'Input',
+      componentProps: {
+        placeholder: '请输入订单编号',
+      },
+    },
+    {
+      fieldName: 'status',
+      label: '售后状态',
+      component: 'Select',
+      componentProps: {
+        options: getDictOptions(DICT_TYPE.TRADE_AFTER_SALE_STATUS, 'number'),
+        placeholder: '请选择售后状态',
+        allowClear: true,
+      },
+    },
+    {
+      fieldName: 'way',
+      label: '售后方式',
+      component: 'Select',
+      componentProps: {
+        options: getDictOptions(DICT_TYPE.TRADE_AFTER_SALE_WAY, 'number'),
+        placeholder: '请选择售后方式',
+        allowClear: true,
+      },
+    },
+    {
+      fieldName: 'type',
+      label: '售后类型',
+      component: 'Select',
+      componentProps: {
+        options: getDictOptions(DICT_TYPE.TRADE_AFTER_SALE_TYPE, 'number'),
+        placeholder: '请选择售后类型',
+        allowClear: true,
+      },
+    },
+    {
+      fieldName: 'createTime',
+      label: '创建时间',
+      component: 'RangePicker',
+      componentProps: {
+        ...getRangePickerDefaultProps(),
+        allowClear: true,
+      },
+    },
+  ];
+}
+
 /** 表格列配置 */
 export function useGridColumns(): VxeGridPropTypes.Columns {
   return [
@@ -92,55 +107,44 @@ export function useGridColumns(): VxeGridPropTypes.Columns {
       field: 'no',
       title: '退款编号',
       fixed: 'left',
+      minWidth: 200,
     },
     {
       field: 'orderNo',
       title: '订单编号',
       fixed: 'left',
+      minWidth: 200,
       slots: { default: 'orderNo' },
     },
     {
-      field: 'spuName',
-      title: '商品名称',
-      align: 'left',
-      minWidth: 200,
-    },
-    {
-      field: 'picUrl',
-      title: '商品图片',
-      cellRender: {
-        name: 'CellImage',
-      },
-    },
-    {
-      field: 'properties',
-      title: '商品属性',
-      minWidth: 200,
-      formatter: ({ cellValue }) => {
-        return cellValue && cellValue.length > 0
-          ? cellValue
-              .map((item: any) => `${item.propertyName} : ${item.valueName}`)
-              .join('\n')
-          : '-';
-      },
+      field: 'productInfo',
+      title: '商品信息',
+      minWidth: 600,
+      slots: { default: 'productInfo' },
     },
     {
       field: 'refundPrice',
       title: '订单金额',
+      width: 120,
       formatter: 'formatAmount2',
     },
     {
       field: 'user.nickname',
       title: '买家',
+      align: 'center',
+      minWidth: 120,
     },
     {
       field: 'createTime',
       title: '申请时间',
+      width: 180,
       formatter: 'formatDateTime',
     },
     {
-      field: 'content',
+      field: 'status',
       title: '售后状态',
+      width: 100,
+      align: 'center',
       cellRender: {
         name: 'CellDictTag',
         props: {
@@ -151,6 +155,8 @@ export function useGridColumns(): VxeGridPropTypes.Columns {
     {
       field: 'way',
       title: '售后方式',
+      width: 100,
+      align: 'center',
       cellRender: {
         name: 'CellDictTag',
         props: {
@@ -160,8 +166,9 @@ export function useGridColumns(): VxeGridPropTypes.Columns {
     },
     {
       title: '操作',
-      width: 80,
+      width: 160,
       fixed: 'right',
+      align: 'center',
       slots: { default: 'actions' },
     },
   ];
