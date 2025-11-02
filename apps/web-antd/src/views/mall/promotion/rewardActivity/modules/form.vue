682a63bc6e5fc31a83ed4a4874896b1822cd5de3
YunaiV <zhijiantianya@gmail.com>
2025-11-02 08:20:11 +0800

    feat：【antd】【mall】满减送的建议

--- apps/web-antd/src/views/mall/promotion/rewardActivity/modules/form.vue
+++ apps/web-antd/src/views/mall/promotion/rewardActivity/modules/form.vue
@@ -27,6 +27,7 @@ import RewardRule from './reward-rule.vue';
 const emit = defineEmits(['success']);
 
 const formData = ref<MallRewardActivityApi.RewardActivity>({
+  // TODO @puhui999：这里的 conditionType、productScope 是不是可以删除呀。因为 data.ts 已经搞了 defaultValue；
   conditionType: PromotionConditionTypeEnum.PRICE.type,
   productScope: PromotionProductScopeEnum.ALL.scope,
   rules: [],
@@ -37,6 +38,8 @@ const getTitle = computed(() => {
     ? $t('ui.actionTitle.edit', ['满减送'])
     : $t('ui.actionTitle.create', ['满减送']);
 });
+
+const rewardRuleRef = ref<InstanceType<typeof RewardRule>>();
 
 const [Form, formApi] = useVbenForm({
   commonConfig: {
@@ -50,8 +53,6 @@ const [Form, formApi] = useVbenForm({
   showDefaultActions: false,
 });
 
-const rewardRuleRef = ref<InstanceType<typeof RewardRule>>();
-
 // TODO @芋艿：这里需要在简化下；
 const [Modal, modalApi] = useVbenModal({
   async onConfirm() {
@@ -60,30 +61,27 @@ const [Modal, modalApi] = useVbenModal({
       return;
     }
     modalApi.lock();
+    // 提交表单
     try {
       const data = await formApi.getValues();
-
       rewardRuleRef.value?.setRuleCoupon();
-
       if (data.startAndEndTime && Array.isArray(data.startAndEndTime)) {
         data.startTime = data.startAndEndTime[0];
         data.endTime = data.startAndEndTime[1];
         delete data.startAndEndTime;
       }
-
       data.rules?.forEach((item: any) => {
         item.discountPrice = convertToInteger(item.discountPrice || 0);
         if (data.conditionType === PromotionConditionTypeEnum.PRICE.type) {
           item.limit = convertToInteger(item.limit || 0);
         }
       });
-
       setProductScopeValues(data);
 
       await (formData.value?.id
         ? updateRewardActivity(data as MallRewardActivityApi.RewardActivity)
         : createRewardActivity(data as MallRewardActivityApi.RewardActivity));
-
+      // 关闭并提示
       await modalApi.close();
       emit('success');
       message.success($t('ui.actionMessage.operationSuccess'));
@@ -100,26 +98,23 @@ const [Modal, modalApi] = useVbenModal({
       };
       return;
     }
-
+    // 加载数据
     const data = modalApi.getData<MallRewardActivityApi.RewardActivity>();
     if (!data || !data.id) {
       return;
     }
-
     modalApi.lock();
     try {
       const result = await getReward(data.id);
-
       result.startAndEndTime = [result.startTime, result.endTime] as any[];
-
       result.rules?.forEach((item: any) => {
         item.discountPrice = formatToFraction(item.discountPrice || 0);
         if (result.conditionType === PromotionConditionTypeEnum.PRICE.type) {
           item.limit = formatToFraction(item.limit || 0);
         }
       });
-
       formData.value = result;
+      // 设置到 values
       await formApi.setValues(result);
 
       await getProductScope();
@@ -153,6 +148,7 @@ async function getProductScope() {
   }
 }
 
+// TODO @puhui999：/Users/yunai/Java/yudao-ui-admin-vben-v5/apps/web-antd/src/views/mall/promotion/coupon/template/data.ts 可以类似 /Users/yunai/Java/yudao-ui-admin-vben-v5/apps/web-antd/src/views/mall/promotion/coupon/template/data.ts 的 productScopeValues（微信交流）
 function setProductScopeValues(data: any) {
   switch (formData.value.productScope) {
     case PromotionProductScopeEnum.CATEGORY.scope: {
