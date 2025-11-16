bb749844fe77b497684133566fbb036fb0b0cbb4
YunaiV <zhijiantianya@gmail.com>
2025-11-16 18:41:46 +0800

    feat：【ele】【erp】stock 的迁移（100%）- move、out

--- apps/web-antd/src/views/erp/stock/out/data.ts
+++ apps/web-antd/src/views/erp/stock/out/data.ts
@@ -331,7 +331,7 @@ export function useGridColumns(): VxeTableGridOptions['columns'] {
     },
     {
       title: '操作',
-      width: 220,
+      width: 260,
       fixed: 'right',
       slots: { default: 'actions' },
     },
