f429e74e7963b8ed93b3ecc3d21545826f744997
puhui999 <puhui999@163.com>
2025-12-20 08:56:32 +0800

    feat：【antd】【mall】point 优化完善

--- apps/web-antd/src/views/mall/promotion/point/components/index.ts
+++ apps/web-antd/src/views/mall/promotion/point/components/index.ts
@@ -1,2 +1,2 @@
-export { default as PointShowcase } from './point-showcase.vue';
-export { default as PointTableSelect } from './point-table-select.vue';
+export { default as PointShowcase } from './showcase.vue';
+export { default as PointTableSelect } from './table-select.vue';
