4adce844d3e2172e77b0e88fb3eda6ac9e91f645
YunaiV <zhijiantianya@gmail.com>
2026-05-17 10:50:34 +0800

    feat（wms）：增加 merchant 模块的迁移

---
+++ apps/web-antd/src/views/wms/md/merchant/components/index.ts
@@ -0,0 +1,1 @@
+export { default as WmsMerchantSelect } from './merchant-select.vue';
---
+++ apps/web-antd/src/views/wms/md/merchant/modules/form.vue
@@ -0,0 +1,94 @@
+<script lang="ts" setup>
+import type { WmsMerchantApi } from '#/api/wms/md/merchant';
+
+import { computed, ref } from 'vue';
+
+import { useVbenModal } from '@vben/common-ui';
+
+import { message } from 'ant-design-vue';
+
+import { useVbenForm } from '#/adapter/form';
+import {
+  createMerchant,
+  getMerchant,
+  updateMerchant,
+} from '#/api/wms/md/merchant';
+import { $t } from '#/locales';
+
+import { useFormSchema } from '../data';
+
+defineOptions({ name: 'WmsMerchantForm' });
+
+const emit = defineEmits(['success']);
+const formData = ref<WmsMerchantApi.Merchant>();
+const getTitle = computed(() => {
+  return formData.value?.id
+    ? $t('ui.actionTitle.edit', ['往来企业'])
+    : $t('ui.actionTitle.create', ['往来企业']);
+});
+
+const [Form, formApi] = useVbenForm({
+  commonConfig: {
+    componentProps: {
+      class: 'w-full',
+    },
+    labelWidth: 120,
+  },
+  wrapperClass: 'grid-cols-2',
+  layout: 'horizontal',
+  schema: [],
+  showDefaultActions: false,
+});
+
+/** 表单 schema 需要 formApi 引用，所以通过 setState 设置 schema */
+formApi.setState({ schema: useFormSchema(formApi) });
+
+const [Modal, modalApi] = useVbenModal({
+  async onConfirm() {
+    const { valid } = await formApi.validate();
+    if (!valid) {
+      return;
+    }
+    modalApi.lock();
+    // 提交表单
+    const data = (await formApi.getValues()) as WmsMerchantApi.Merchant;
+    try {
+      await (formData.value?.id
+        ? updateMerchant(data)
+        : createMerchant(data));
+      // 关闭并提示
+      await modalApi.close();
+      emit('success');
+      message.success($t('ui.actionMessage.operationSuccess'));
+    } finally {
+      modalApi.unlock();
+    }
+  },
+  async onOpenChange(isOpen: boolean) {
+    if (!isOpen) {
+      formData.value = undefined;
+      return;
+    }
+    await formApi.resetForm();
+    const data = modalApi.getData<WmsMerchantApi.Merchant>();
+    if (!data || !data.id) {
+      return;
+    }
+    // 加载数据
+    modalApi.lock();
+    try {
+      formData.value = await getMerchant(data.id);
+      // 设置到 values
+      await formApi.setValues(formData.value);
+    } finally {
+      modalApi.unlock();
+    }
+  },
+});
+</script>
+
+<template>
+  <Modal :title="getTitle" class="w-1/2">
+    <Form class="mx-4" />
+  </Modal>
+</template>
